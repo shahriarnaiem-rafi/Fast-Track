e435c3b6d161e3621891e140a02f10fd0844ccbd
shahriarnaiem-rafi <shahriarnaiemrafi33.com>
2024-12-29 12:45:12 +0600

    feature updated dashboard

--- adminpannel/pages/index.php
+++ adminpannel/pages/index.php
@@ -4,7 +4,7 @@
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Admin  Dashboard</title>
+  <title>Admin Dashboard</title>
   <!--begin::Primary Meta Tags-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="title" content="AdminLTE v4 | Dashboard" />
@@ -266,94 +266,76 @@
 
 
           <div class="container-fluid dashboard-show">
-            <div class="row">
-              <!--begin::Col (First row, first column)-->
-              <div class="col-lg-6 col-12"> <!-- Adjusted to col-lg-6 to fit two boxes per row -->
-                <!--begin::Small Box Widget 1-->
-                <div class="small-box text-bg-primary" style="width: 70%; height: 210px; margin: 0 auto; margin-bottom: 20px;"> <!-- Updated height to 210px -->
-                  <div class="inner">
-                    <h3>150</h3>
-                    <p>Parsel Received</p>
-                  </div>
-                  <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"></path>
-                  </svg>
-                  <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                    More info <i class="bi bi-link-45deg"></i>
-                  </a>
-                </div>
-                <!--end::Small Box Widget 1-->
-              </div>
-              <!--end::Col-->
+          <div class="row">
+  <!-- First Row -->
+  <div class="col-lg-6 col-12">
+    <div class="small-box text-bg-primary" style="width: 80%; height: 210px; margin: 0 auto; margin-bottom: 20px;">
+      <div class="inner">
+        <h3>150</h3>
+        <p>Parsel Received</p>
+      </div>
+      <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c..."></path>
+      </svg>
+      <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+        More info <i class="bi bi-link-45deg"></i>
+      </a>
+    </div>
+  </div>
+  <div class="col-lg-6 col-12">
+    <div class="small-box text-bg-success" style="width: 80%; height: 210px; margin: 0 auto; margin-bottom: 20px;">
+      <div class="inner">
+        <h3>400</h3>
+        <p>Old Parsel</p>
+      </div>
+      <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c..."></path>
+      </svg>
+      <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+        More info <i class="bi bi-link-45deg"></i>
+      </a>
+    </div>
+  </div>
+</div>
 
-              <!--begin::Col (First row, second column)-->
-              <div class="col-lg-6 col-12"> <!-- Adjusted to col-lg-6 to fit two boxes per row -->
-                <!--begin::Small Box Widget 2-->
-                <div class="small-box text-bg-success" style="width: 70%; height: 210px; margin: 0 auto; margin-bottom: 20px;"> <!-- Updated height to 210px -->
-                  <div class="inner">
-                    <h3>400</h3>
-                    <p>Old Parsel</p>
-                  </div>
-                  <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
-                  </svg>
-                  <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                    More info <i class="bi bi-link-45deg"></i>
-                  </a>
-                </div>
-                <!--end::Small Box Widget 2-->
-              </div>
-              <!--end::Col-->
-            </div>
+<!-- Second Row -->
+<div class="row">
+  <div class="col-lg-6 col-12">
+    <div class="small-box text-bg-warning" style="width: 80%; height: 210px; margin: 0 auto; margin-bottom: 20px;">
+      <div class="inner">
+        <h3>44</h3>
+        <p>Total Parsel</p>
+      </div>
+      <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25..."></path>
+      </svg>
+      <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+        More info <i class="bi bi-link-45deg"></i>
+      </a>
+    </div>
+  </div>
+  <div class="col-lg-6 col-12">
+    <div class="small-box text-bg-danger" style="width: 80%; height: 210px; margin: 0 auto; margin-bottom: 20px;">
+      <div class="inner">
+        <h3>65</h3>
+        <p>Today Delivered</p>
+      </div>
+      <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25..."></path>
+      </svg>
+      <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+        More info <i class="bi bi-link-45deg"></i>
+      </a>
+    </div>
+  </div>
+</div>
 
-            <!-- Second Row -->
-            <div class="row">
-              <!--begin::Col (Second row, first column)-->
-              <div class="col-lg-6 col-12"> <!-- Adjusted to col-lg-6 to fit two boxes per row -->
-                <!--begin::Small Box Widget 3-->
-                <div class="small-box text-bg-warning" style="width: 70%; height: 210px; margin: 0 auto; margin-bottom: 20px;"> <!-- Updated height to 210px -->
-                  <div class="inner">
-                    <h3>44</h3>
-                    <p>Total Parsel</p>
-                  </div>
-                  <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                  </svg>
-                  <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                    More info <i class="bi bi-link-45deg"></i>
-                  </a>
-                </div>
-                <!--end::Small Box Widget 3-->
-              </div>
-              <!--end::Col-->
-
-              <!--begin::Col (Second row, second column)-->
-              <div class="col-lg-6 col-12"> <!-- Adjusted to col-lg-6 to fit two boxes per row -->
-                <!--begin::Small Box Widget 4-->
-                <div class="small-box text-bg-danger" style="width: 70%; height: 210px; margin: 0 auto; margin-bottom: 20px;"> <!-- Updated height to 210px -->
-                  <div class="inner">
-                    <h3>65</h3>
-                    <p>Today Delivered</p>
-                  </div>
-                  <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                  </svg>
-                  <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                    More info <i class="bi bi-link-45deg"></i>
-                  </a>
-                </div>
-                <!--end::Small Box Widget 4-->
-              </div>
-              <!--end::Col-->
-            </div>
 
 
 
           </div>
           <!--end::Row-->
-          
+
 
 
           <!-- graph -->
