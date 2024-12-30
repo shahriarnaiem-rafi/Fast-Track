f54c5dbeea429eba5253f75393ee6cea316603ee
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2024-12-30 21:24:58 +0600

    all feature added

--- adminpannel/connect/aside.php
+++ adminpannel/connect/aside.php
@@ -2,7 +2,7 @@
   <!--begin::Sidebar Brand-->
   <div class="sidebar-brand">
     <!--begin::Brand Link-->
-    <a href="./index.html" class="brand-link">
+    <a href="./index.php" class="brand-link">
       <!--begin::Brand Image-->
       <img
         src="../assets/img/AdminLTELogo.png"
